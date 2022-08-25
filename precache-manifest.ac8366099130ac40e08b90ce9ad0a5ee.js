227e463a33fe58bdfde29b61424e0f2e636c0737
Izzan Silmi Aziz <izzan.abad@gmail.com>
2022-08-25 22:45:15 +0700

    Updates

---
+++ precache-manifest.ac8366099130ac40e08b90ce9ad0a5ee.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bf2b56a064c58fd595967d073802eaa6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9d1e38c8d988e4718e1f",
+    "url": "/static/css/main.d498bfb3.chunk.css"
+  },
+  {
+    "revision": "b755427842f68449c5ca",
+    "url": "/static/js/2.ea305df0.chunk.js"
+  },
+  {
+    "revision": "1741560655629be7700f6f723b05ba9a",
+    "url": "/static/js/2.ea305df0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9d1e38c8d988e4718e1f",
+    "url": "/static/js/main.090b6991.chunk.js"
+  },
+  {
+    "revision": "985f8210b99ee79e0bb4",
+    "url": "/static/js/runtime-main.e4922375.js"
+  },
+  {
+    "revision": "38d369e823b9ab02ff6ab912cebe71a2",
+    "url": "/static/media/Google.38d369e8.png"
+  },
+  {
+    "revision": "0b4742bbed3a35e063bf7c3b91584a7d",
+    "url": "/static/media/cats.0b4742bb.png"
+  },
+  {
+    "revision": "dabc23cf9925aa93cbba860cfa607e18",
+    "url": "/static/media/cosmetic.dabc23cf.png"
+  },
+  {
+    "revision": "02be168eeeff23cd7feab294f2185729",
+    "url": "/static/media/credit_card.02be168e.png"
+  },
+  {
+    "revision": "18c7be41365ae30e4a29e572acb1c364",
+    "url": "/static/media/doctor.18c7be41.png"
+  },
+  {
+    "revision": "b7a4bcf7c4455d5cbd64220b6cd3884c",
+    "url": "/static/media/dome.b7a4bcf7.png"
+  },
+  {
+    "revision": "7fd85796454a90a4be4e80efdec59368",
+    "url": "/static/media/gp.7fd85796.png"
+  },
+  {
+    "revision": "5536f5bd9c57d1025287e5ccdd33e85b",
+    "url": "/static/media/mask.5536f5bd.png"
+  },
+  {
+    "revision": "549ccf31f0eb6284b69374af134d4152",
+    "url": "/static/media/money.549ccf31.png"
+  },
+  {
+    "revision": "17cd24723ca666a0d9b1c69e91c23b56",
+    "url": "/static/media/netflix.17cd2472.png"
+  },
+  {
+    "revision": "e1770bd1f8d2791af5277e656b9ad516",
+    "url": "/static/media/save.e1770bd1.png"
+  },
+  {
+    "revision": "88768ebfe19bd96eccf1bcf770a8c8b7",
+    "url": "/static/media/scala.88768ebf.png"
+  },
+  {
+    "revision": "1a4d4e9666f944054e4c9e075c2aa123",
+    "url": "/static/media/ws.1a4d4e96.png"
+  }
+]);
